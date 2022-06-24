2804b231339f0f5eee07b20911e2166e0a6a72b8
Delaney Fogarty <delaney.fk@gmail.com>
2022-06-24 10:30:49 -0700

    first test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,42 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const mockUser = {
+  firstName: 'Delaney',
+  lastName: 'User',
+  email: 'randomUser@dod.gov',
+  password: '54321',
+};
+
+const signUpAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  const agent = request.agent(app);
+
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('new user created upon sign up', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { firstName, lastName, email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
+
   afterAll(() => {
     pool.end();
   });
